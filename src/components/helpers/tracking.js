109901b2dc4f6bcfa8d40dec8e8079ca3540ce60
gtstride <lewisagho47@gmail.com>
2021-03-24 12:30:24 +0100

    Removed eslint, typescript-eslint and added react-ga, node-sass to package.json, readded the tracking.js file, corrected the country in UZIcare and also corrected things in the AutoComplete and Pricing.jsx file

--- src/components/helpers/tracking.js
+++ src/components/helpers/tracking.js
@@ -1,34 +1,34 @@
-// import ReactGA from "react-ga";
+import ReactGA from "react-ga";
 
-// export const initGA = (trackingID) => {
-//   ReactGA.initialize(trackingID);
-// };
+export const initGA = (trackingID) => {
+  ReactGA.initialize(trackingID);
+};
 
-// export const PageView = () => {
-//   ReactGA.pageview(window.location.href);
-// };
+export const PageView = () => {
+  ReactGA.pageview(window.location.href);
+};
 
-// /**
-//  * Event - Add custom tracking event.
-//  * @param {string} category
-//  * @param {string} action
-//  * @param {string} label
-//  */
-// export const Event = (category, action, label) => {
-//   ReactGA.event({
-//     category: category,
-//     action: action,
-//     label: label,
-//   });
-// };
+/**
+ * Event - Add custom tracking event.
+ * @param {string} category
+ * @param {string} action
+ * @param {string} label
+ */
+export const Event = (category, action, label) => {
+  ReactGA.event({
+    category: category,
+    action: action,
+    label: label,
+  });
+};
 
-// export const setProperties = ({ userId, name }) => {
-//   ReactGA.set({
-//     appName: "UZI-Logistics-and-Trucking Website",
-//     hostname: window.location.hostname,
-//     location: window.location.href,
-//     language: localStorage.locale,
-//     userId,
-//     appId: name,
-//   });
-// };
+export const setProperties = ({ userId, name }) => {
+  ReactGA.set({
+    appName: "UZI-Logistics-and-Trucking Website",
+    hostname: window.location.hostname,
+    location: window.location.href,
+    language: localStorage.locale,
+    userId,
+    appId: name,
+  });
+};
